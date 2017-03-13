5d0c5b9994771a30d73e36b39d7b1288791db079
Jakub Wierzchoslawski <jakub.wierzchoslawski@gmail.com>
2017-03-13 12:24:35 +0100

    fixed for local run

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
